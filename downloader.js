9ad705cd7a64314b89b50df6be054cc4ae98d13b
CarlosEDP <me@carlosedp.com>
2011-01-23 13:00:43 -0200

    Tranformed downloader into a function.

--- downloader.js
+++ downloader.js
@@ -5,36 +5,41 @@ var sys = require("sys"),
     fs = require("fs"),
     events = require("events");
 
-var requestUrl = "http://python.org/ftp/python/3.1.3/python-3.1.3-macosx10.3.dmg";
-//var requestUrl = "http://nodejs.org/dist/node-v0.2.6.tar.gz";
+
+function downloadFile(requestUrl) {
+    var host = url.parse(requestUrl).hostname;
+    var filename = url.parse(requestUrl).pathname.split("/").pop();
+
+    var theurl = http.createClient(80, host);
+    sys.puts("Downloading file: " + filename);
+    var request = theurl.request('GET', requestUrl, {"host": host});
+    request.end();
+
+    var dlprogress = 0;
+
+    setInterval(function () {
+        sys.puts("Download progress for file" + filename + ": " + dlprogress + " bytes");
+    }, 1000);
 
 
-var host = url.parse(requestUrl).hostname
-var filename = url.parse(requestUrl).pathname.split("/").pop()
+    request.addListener('response', function (response) {
+        var downloadfile = fs.createWriteStream(filename, {'flags': 'a'});
+        sys.puts("File size " + filename + ": " + response.headers['content-length'] + " bytes.");
+        response.addListener('data', function (chunk) {
+            dlprogress += chunk.length;
+            downloadfile.write(chunk, encoding='binary');
+        });
+        response.addListener("end", function() {
+            downloadfile.end();
+            sys.puts("Finished downloading " + filename);
+        });
 
-var theurl = http.createClient(80, host);
-sys.puts("Downloading file: " + filename);
-var request = theurl.request('GET', requestUrl, {"host": host});
-request.end();
-
-var dlprogress = 0;
-
-setInterval(function () {
-   sys.puts("Download progress: " + dlprogress + " bytes");
-}, 1000);
-
-
-request.addListener('response', function (response) {
-    var downloadfile = fs.createWriteStream(filename, {'flags': 'a'});
-    sys.puts("File size: " + response.headers['content-length'] + " bytes.")
-    response.addListener('data', function (chunk) {
-        dlprogress += chunk.length;
-        downloadfile.write(chunk, encoding='binary');
     });
-    response.addListener("end", function() {
-        downloadfile.end();
-        sys.puts("Download finished");
-    });
+}
 
-});
+var url1 = "http://python.org/ftp/python/3.1.3/python-3.1.3-macosx10.3.dmg";
+var url2 = "http://nodejs.org/dist/node-v0.2.6.tar.gz";
+
+downloadFile(url1);
+downloadFile(url2);
 
