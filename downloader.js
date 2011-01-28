b131d851af7c66b0b697225cb8fc223da89a8b2c
CarlosEDP <me@carlosedp.com>
2011-01-28 18:15:40 -0200

    New structure for server app

--- downloader.js
+++ downloader.js
@@ -1,36 +1,46 @@
 var http = require("http");
 var url = require("url");
 var fs = require("fs");
+var sys = require("sys");
 
 var downloadDir =  __dirname + '/downloadedFiles/';
+
+
+
 
 exports.DownloadFile = function (requestUrl) {
     var host = url.parse(requestUrl).hostname;
     var filename = url.parse(requestUrl).pathname.split("/").pop();
+    var dlprogress = 0;
 
     var theurl = http.createClient(80, host);
     console.log("Downloading file: " + filename);
-    var request = theurl.request('GET', requestUrl, {"host": host});
-    request.end();
-
-    var dlprogress = 0;
+    var request = theurl.request('GET', requestUrl, {"host": host});  
 
     var downloadId = setInterval(function () {
             console.log("Download progress for file" + filename + ": " + dlprogress + " bytes");
             }, 1000);
 
-
-    request.addListener('response', function (response) {
-            var downloadfile = fs.createWriteStream(downloadDir + filename, {'flags': 'a'});
-            console.log("File size " + filename + ": " + response.headers['content-length'] + " bytes.");
-            response.addListener('data', function (chunk) {
-                dlprogress += chunk.length;
-                downloadfile.write(chunk, encoding='binary');
-                });
-            response.addListener("end", function() {
-                downloadfile.end();
-                console.log("Finished downloading " + filename);
-                clearInterval(downloadId);
-                });
-            });
+    request.on('response', function(response) {
+        var filesize = response.headers['content-length'];
+        console.log("File size " + filename + ": " + filesize + " bytes.");
+        response.pause();
+        if (filesize >= 50000) {
+            // TODO -> Cancel file download
+            console.log("Download cancelled. File too big.");
+        } else {
+            response.resume();
+        }
+        var downloadfile = fs.createWriteStream(downloadDir + filename, {'flags': 'a'});
+        response.on('data', function (chunk) {
+            dlprogress += chunk.length;
+            downloadfile.write(chunk, encoding='binary');
+        });
+        response.on('end', function() {
+            downloadfile.end();
+            console.log("Finished downloading " + filename);
+            clearInterval(downloadId);   
+        });
+    });
+    request.end();
 };
--- server.js
+++ server.js
@@ -6,49 +6,52 @@ var sys = require('sys');
 // Custom libraries
 var downloader = require('./downloader');
 
-// Server port
+// Server Configuration
 var port = (process.env.PORT || 8000);
-
-// Path to static directory
 var pub = __dirname + '/static';
+var Settings = { development: {}, test: {}, production: {} };
 
-var server = express.createServer(
-        express.bodyDecoder(),
-        connect.staticProvider(pub),
-        express.cookieDecoder(),
-        express.session({ secret: 'secret stuff' })
-);
+var server = express.createServer();
+
+server.configure('development', function() {
+  //server.set('db-uri', 'mongodb://localhost/nodepad-development');
+  server.use(express.errorHandler({ dumpExceptions: true }));  
+});
+
+//server.configure('test', function() {
+  //server.set('db-uri', 'mongodb://localhost/nodepad-test');
+//});
+
+//server.configure('production', function() {
+  //server.set('db-uri', 'mongodb://localhost/nodepad-production');
+//});
 
 server.configure(function(){
+        server.use(express.favicon());
+        server.use(express.bodyDecoder());
+        server.use(express.cookieDecoder());
+        server.use(express.session({secret: 'secret stuff'}));
+        server.use(express.logger({ format: '\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :response-time ms' }));
+        server.use(express.methodOverride());
+        server.use(server.router);
+        server.use(express.staticProvider(pub));
         server.set('views', __dirname + '/views');
         server.set('view engine', 'jade');
-        server.use(server.router);
+
 });
 
 server.helpers(require('./helpers.js').helpers);
 server.dynamicHelpers(require('./helpers.js').dynamicHelpers);
 
-//setup the errors
-//server.error(function(err, req, res, next){
-        //if (err instanceof NotFound) {
-            //res.render('404', { locals: {
-                //title : '404 - Not Found',
-                //description: '',
-                //author: '',
-                //analyticssiteid: 'XXXXXXX'
-            //},status: 404 });
-        //} else {
-            //res.render('500', { locals: {
-                //title : 'The Server Encountered an Error',
-                //description: '',
-                //author: '',
-                //analyticssiteid: 'XXXXXXX',
-                //error: err
-            //},status: 500 });
-        //}
-//});
+function isUrl(url) {
+	var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+	return regexp.test(url);
+}
 
-server.listen(port);
+function isEmail(email) {
+   var regexp = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+   return regexp.test(email);
+}
 
 ///////////////////////////////////////////
 //              Routes                   //
@@ -68,26 +71,63 @@ server.get('/', function(request, response) {
 });
 
 server.post('/', function(request, response) {
-    console.log("Received request to download file: " + request.body.URL);
-    downloader.DownloadFile(request.body.URL);
-    request.flash('info', 'Download scheduled');
+    console.log("Received request to download file: " + request.body.download);
+    if (!isUrl(request.body.download)) {
+        request.flash('error', 'Invalid URL');
+        console.log("Invalid URL");
+    } else {
+        if (!isEmail(request.body.email)) {
+            request.flash('error', 'Invalid Email');
+            console.log("Invalid Email");
+        } else {
+            request.flash('info', 'Download scheduled');
+            downloader.DownloadFile(request.body.download);
+        }
+    }
     response.redirect('back');
 });
 
-//A Route for Creating a 500 Error (Useful to keep around)
-server.get('/500', function(req, res){
-    throw new Error('This is a 500 Error');
-});
-
-//The 404 Route (ALWAYS Keep this as the last route)
-server.get('/*', function(req, res) {
-    throw new NotFound;
-});
-
-function NotFound(msg){
-    this.name = 'NotFound';
-    Error.call(this, msg);
-    Error.captureStackTrace(this, arguments.callee);
+// Error handling
+function NotFound(msg) {
+  this.name = 'NotFound';
+  Error.call(this, msg);
+  Error.captureStackTrace(this, arguments.callee);
 }
 
-console.log('Listening on http://0.0.0.0:' + port );
+sys.inherits(NotFound, Error);
+
+server.get('/404', function(req, res) {
+  throw new NotFound;
+});
+
+server.get('/500', function(req, res) {
+  throw new Error('An expected error');
+});
+
+server.get('/bad', function(req, res) {
+  unknownMethod();
+});
+
+server.error(function(err, req, res, next) {
+  if (err instanceof NotFound) {
+    res.render('404', { status: 404 });
+  } else {
+    next(err);
+  }
+});
+
+server.error(function(err, req, res) {
+  res.render('500', {
+    status: 500,
+    locals: {
+      error: err
+    } 
+  });
+});
+
+/////////// Run Server ///////////
+if (!module.parent) {
+  server.listen(port);
+  console.log("Express server listening on port %d, environment: %s", server.address().port, server.settings.env);
+}
+
