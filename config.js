5baf1395728032225674a0a86d8b75d60c090909
CarlosEDP <me@carlosedp.com>
2011-02-18 18:54:57 -0200

    Added config file and downloads list

---
+++ config.js
@@ -0,0 +1,9 @@
+// Server Configuration
+exports.serverPort = (process.env.PORT || 8000);
+exports.DBserverAddress = 'localhost';
+exports.pub = __dirname + '/static';
+exports.serverAddress = 'http://localhost:' + this.serverPort;
+
+exports.downloadDirSuffix = '/downloadedFiles/';
+exports.downloadDir = __dirname + this.downloadDirSuffix;
+
